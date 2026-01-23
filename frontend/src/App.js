41fc10d84f42a71d91d0f5c02001b2df62ece28f
omar-rifai <omar.void@gmail.com>
2026-01-23 15:09:50 +0100

    Update site banner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,23 +25,24 @@ function App() {
               display: "flex", alignItems: "center", justifyContent: "center", gap: 2,
               padding: "8px 16px", backgroundColor: "#f2f4f6ff", color: "black", 
             }}>
-            <Box component="img" src="logo-safepaw.jpg" sx={{ height: 50, width: "auto", ml: -50 }}></Box>
+            <Box component="img" src="logo_safepaw.jpg" sx={{ height: 50, width: "auto", ml: -40 }}></Box>
             <Typography variant="h4" sx={{
               mx: 2, fontFamily: "'Montserrat', sans-serif", fontWeight: 700,
               letterSpacing: "0.05em", color: "#333333"
             }}>SAFEPAW</Typography>
-            <Box component="img" src="logo_polytech.png" sx={{ height: 50, width: "auto" }}></Box>
+            <Box component="img" src="logo_anr.png" sx={{ height: 30, width: "auto" }}></Box>
+            <Box component="img" src="logo_univ-tours.png" sx={{ height: 30, width: "auto" }}></Box>
             <Box component="img" src="logo_emse.png" sx={{ height: 50, width: "auto" }}></Box>
           </Box>
           <Divider textAlign="left" sx={{ my: 2, '&::before': { width: '5%' }  }}>  <Typography variant="body1"> France's Maternity Facilities</Typography> </Divider>
-          <Box sx={{ display: 'flex', gap: 2 }}>
+          <Box sx={{ display: 'flex', gap: 2, alignItems: 'stretch', height: 600 }}>
             <Box sx={{ width: '20%', position: 'relative' }}>
               <InputForm />
             </Box>
-            <Box sx={{ width: '40%', position: 'relative' }}>
+            <Box sx={{ width: '40%', height: '100%', position: 'relative' }}>
               <CustomMap inputData={inputData} outputData={outputData} />
             </Box>
-            <Box sx={{ width: '40%', position: 'relative' }}>
+            <Box sx={{ width: '40%', height: '100%', position: 'relative' }}>
               <DashboardForm
                 inputData={inputData}
               />
