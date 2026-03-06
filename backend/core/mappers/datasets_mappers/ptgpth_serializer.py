9181ac998ca9daa3fe232600a40d1a32cc9f2ff1
omar-rifai <omar.void@gmail.com>
2026-03-06 16:08:21 +0100

    Update ptgpth serializer with full m_hl function

--- backend/core/mappers/datasets_mappers/ptgpth_serializer.py
+++ backend/core/mappers/datasets_mappers/ptgpth_serializer.py
@@ -3,6 +3,14 @@ from backend.core.data_models.input_models import Facility, Region, Instance, Re
 import geopandas as gpd
 
 specialities = ["CSC", "DERMA", "RHUMA", "URO", "GASTRO", "OPH", "ENDO", "ORL"]
+post_op_scenarios = {"DAY_HC":{"PTG_HC":28,"PTH_HC":21,"PTG_DOM":0,"PTH_DOM":0,"PTG_HC_HDJ":21,"PTH_HC_HDJ":14,"PTG_HDJ":0,"PTH_HDJ":0},
+                     "KINE_SSR":{"PTG_HC":0,"PTH_HC":0,"PTG_DOM":0,"PTH_DOM":0,"PTG_HC_HDJ":20,"PTH_HC_HDJ":15,"PTG_HDJ":25,"PTH_HDJ":20},
+                     "KINE_DOM":{"PTG_HC":0,"PTH_HC":0,"PTG_DOM":25,"PTH_DOM":20,"PTG_HC_HDJ":0,"PTH_HC_HDJ":0,"PTG_HDJ":0,"PTH_HDJ":0}}
+
+finance_PTG = {"CHIR/ORTHO":46, "ANES" : 46, "CSC": 34.75, "DERMA": 40,"ENDO": 40, "GASTRO": 40, "GYNECO": 40,
+               "OPH":40, "ORL":40, "RHUMA":40, "URO":40, "KINE_MCO":9.95, "DAY_HC":4365.61, "KINE_DOM":769.56, "KINE_SSR":126.80,"post_ORTH": 370.99}
+finance_PTH = {"CHIR/ORTHO": 46, "ANES": 46, "CSC":34.75, "DERMA":40, "ENDO":40, "GASTRO":40, "GYNECO":40, "OPH":40,"ORL": 40,
+               "RHUMA":40, "URO":40, "KINE_MCO":9.95, "DAY_HC":3966.66, "KINE_DOM":803.30, "KINE_SSR":127.63, "post_ORTH":370.99}
 
 def load_types_parcours(path: str, min_patients: int, dep_code:str):
     """Read and filter TYPES_PARCOURS for Loire."""
@@ -16,9 +24,10 @@ def load_mco_data(path: str, dep_code: str):
     df = df.rename(columns={'420': 'FI_ET'})
     return reduce_MCO_LOIRE(df, dep_code)
 
-def load_ssr_data(path: str, dep_code:str):
+def load_ssr_data(path: str, dep_code: str):
     """Read SSR data and keep Loire SSR_A rows only."""
     df = pd.read_csv(path, sep=";")
+    df = df.rename(columns={'FI': 'FI_ET'})
     return reduce_SSR_LOIRE(df, dep_code)
 
 def load_data(dep_code:str):
@@ -46,7 +55,7 @@ def reduce_MCO_LOIRE(data: pd.DataFrame, dep_code: str) -> pd.DataFrame:
 
 def reduce_SSR_LOIRE(data: pd.DataFrame, dep_code) -> pd.DataFrame:
     """Keep only SSR_A rows corresponding to Loire department"""
-    dept_code = data['FI'].astype(str).apply(lambda s: int(s[:2]) if s[:2].isdigit() else 0)
+    dept_code = data['FI_ET'].astype(str).apply(lambda s: int(s[:2]) if s[:2].isdigit() else 0)
     return data[dept_code.isin(dep_code) & (data['GDE'] == "SSR_A")].reset_index(drop=True)
 
 
@@ -93,19 +102,30 @@ def _get_available_pathways():
     """Returns available pathways for each facility ``type''"""
     return  []
 
-def _get_resources_capacity(nofinesset: str, list_finess: list, df_mco: pd.DataFrame,
-                            df_types_parcours: pd.DataFrame) -> dict:
+def _get_m_hl(list_finess: list, df_types_parcours_init: pd.DataFrame, df_ssr: pd.DataFrame, df_mco: pd.DataFrame,
+                            df_types_parcours: pd.DataFrame, ) -> dict:
     """Returns a dict with each available resource and its capacity given a finess number"""
     
-    _get_ORTH_pre_capacity()
-    _get_ANES_capacity()
+    m_hl = _get_resource_capacity(m_hl, list_finess,  df_mco, df_types_parcours, "ORTH/CHIR", "JLI_CHI", 3)
+    m_hl = _get_resource_capacity(m_hl, list_finess,  df_mco, df_types_parcours, "ANES", "JLI_CHI", 2)
     m_hl = _get_specialities_cap(m_hl, df_mco, df_types_parcours, list_finess)
-    _get_KINE_MCO_capacity()
-    _get_DAY_HC_capacity()
-    _get_KINE_SSR_capacity()
-    _get_ORTH_post_capacity()
-    capacities = {}
-    return capacities
+    m_hl = _get_resource_capacity(m_hl, list_finess,  df_mco, df_types_parcours, "KINE_MCO", "ACTCLI_PM", {"PTG":10, "PTH":15})
+    m_hl = _get_resource_capacity(m_hl, list_finess,  df_ssr,
+                                df_types_parcours_init
+                                    .assign(sej_type = \
+                                            df_types_parcours_init["sej_type"].str.cat(df_types_parcours_init["SSR_TYPE"], sep="_")),
+                                "DAY_HC", "JOUHC", post_op_scenarios["DAY_HC"])
+    m_hl = _get_resource_capacity(m_hl, list_finess,  df_ssr,
+                                df_types_parcours_init
+                                    .assign(sej_type = \
+                                            df_types_parcours_init["sej_type"].str.cat(df_types_parcours_init["SSR_TYPE"], sep="_")),
+                                "KINE_SSR", "JOUHP", post_op_scenarios["KINE_SSR"])
+    m_hl = _get_resource_capacity(m_hl, list_finess, None,
+                                df_types_parcours_init
+                                    .assign(sej_type = \
+                                            df_types_parcours_init["sej_type"].str.cat(df_types_parcours_init["SSR_TYPE"], sep="_")),
+                                "KINE_DOM", None, post_op_scenarios["KINE_DOM"])
+    return m_hl
 
 def _get_specialities_frac(df_mco: pd.DataFrame, list_finess: list) -> dict:
     """Returns a proxy of facilities' capacity for a speciality. When there is no info on availability,
@@ -156,58 +176,42 @@ def _get_specialities_cap(m_hl: dict, df_mco: pd.DataFrame, df_types_parcours: p
     m_hl = _extend_nested_dict(m_hl, cap_specialities)
     return  m_hl
 
+
+
+def _get_resource_capacity(m_hl_init: dict, list_finess: list, df_activity: pd.DataFrame, df_types_parcours: pd.DataFrame,
+                           resource_name: str, resource_table_field: str, resource_consumption: dict) -> dict:
+    """ 
+    Calculates capacity of resource using a proxy of total departmental number of visits times
+    the proportion of resrouce available at a facility (we assume that visit consumes ``resource_consumption'' resources
+    # Hypothesis: all resources except specialists are present in all facilities by type either MCO or SSR   
+    """
+    import math
+    m_hl = {h: {resource_name: 0} for h in list_finess}    
+   
+    if isinstance(resource_consumption, dict):
+        resource_consumption = df_types_parcours["sej_type"].map(resource_consumption)
+    total_consumption_resource = float((resource_consumption * df_types_parcours["nb"]).sum())
+
+    if df_activity is None:
+         m_hl["DOM"][resource_name] = math.ceil(total_consumption_resource)
+    else:  
+        for h in list_finess:
+            if h in df_activity["FI_ET"].unique():
+                    total_dep_resources = df_activity[resource_table_field].sum()
+                    current_facility_resources = df_activity[df_activity["FI_ET"]==h][resource_table_field].iloc[0]
+                    capacity_resource = math.ceil(total_consumption_resource * ( current_facility_resources / total_dep_resources))
+                    m_hl[h][resource_name] = capacity_resource
+            else:
+                current_facility_resources =  m_hl_init[h].get(resource_name, 0)    
+    
+    m_hl = _extend_nested_dict(m_hl_init, m_hl)
+    return m_hl  
+
+
 def _extend_nested_dict(init_dict: dict, ext_dict: dict):
     """Helper function to update capacity of m_hl with an extension containg capacity of new resources"""
     res_dict = {k: init_dict.get(k, {}) | ext_dict[k] for k in set(init_dict) | set(ext_dict)}
     return res_dict
-
-
-def _get_ORTH_pre_capacity(m_hl_init: dict, list_finess: list, df_mco: pd.DataFrame, df_types_parcours: pd.DataFrame) -> dict:
-    """ 
-    Calculates capacity of CHIR./ORTHO. using a proxy of total departmental number of visits times
-    the proportion of beds available at a facility (we assume that visit consumes 3 resources of beds/days
-    # Hypothesis: CHIR./ORTHO. at every MCO-hospital
-    """
-    import math
-    unit_consumption = 3
-    m_hl = {h: {"ORTH/CHIR": 0} for h in list_finess}
-        
-    total_bed_days = df_mco["JLI_CHI"].sum()
-    total_consumption_ORTH = unit_consumption * df_types_parcours["nb"].sum()
-    
-    for h in list_finess:
-        current_facility_beds = df_mco[df_mco["FI_ET"]==h]["JLI_CHI"].iloc[0]
-        capacity_orth = math.ceil(total_consumption_ORTH * ( current_facility_beds / total_bed_days))
-        if current_facility_beds == 0: m_hl[h][j] == 0
-        else: m_hl[h]["ORTH/CHIR"] = capacity_orth
-            
-    m_hl = _extend_nested_dict(m_hl_init, m_hl)
-    return m_hl
-    
-
-def _get_ANES_capacity(m_hl_init: dict, list_finess: list,df_mco: pd.DataFrame, df_types_parcours: pd.DataFrame) -> dict:
-    """ 
-    Calculates capacity of ANES. using a proxy of total departmental number of visits times
-    the proportion of beds available at a facility (we assume that visit consumes 2 resources of beds/days
-    # Hypothesis: ANES. at every MCO-hospital
-    """
-    import math
-    unit_consumption = 2
-    m_hl = {h: {"ANES": 0} for h in list_finess}
-        
-    total_bed_days = df_mco["JLI_CHI"].sum()
-    total_consumption_ORTH = unit_consumption * df_types_parcours["nb"].sum()
-    
-    for h in list_finess:
-        current_facility_beds = df_mco[df_mco["FI_ET"]==h]["JLI_CHI"].iloc[0]
-        capacity_orth = math.ceil(total_consumption_ORTH * ( current_facility_beds / total_bed_days))
-        if current_facility_beds == 0: m_hl[h][j] == 0
-        else: m_hl[h]["ORTH/CHIR"] = capacity_orth
-            
-    m_hl = _extend_nested_dict(m_hl_init, m_hl)
-    return m_hl
-
-
 
 
 def get_Instance(df_instance : pd.DataFrame) -> Instance:
@@ -305,7 +309,7 @@ def get_finness_info(df_mco: pd.DataFrame, df_ssr: pd.DataFrame, gdf_geo: gpd.Ge
                             usecols=["nofinesset", "coordx", "coordy"],  dtype={"coordx": str, "coordy": str},\
                             low_memory=False)
     
-    all_finess = pd.concat([df_ssr["FI"], df_mco["FI_ET"]]).dropna().unique()
+    all_finess = pd.concat([df_ssr["FI_ET"], df_mco["FI_ET"]]).dropna().unique()
     df_finess = df_finess[df_finess["nofinesset"].isin(all_finess)]
     
     transformer = Transformer.from_crs(2154, 4326, always_xy=True)
