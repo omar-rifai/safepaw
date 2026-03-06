f3af789d22ee2b96393b3465f7bf1eb10e21fff6
omar-rifai <omar.void@gmail.com>
2026-03-06 16:22:03 +0100

    Move helper functions to utils module

--- backend/core/mappers/datasets_mappers/ptgptg_utils.py
+++ backend/core/mappers/datasets_mappers/ptgptg_utils.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from backend.core.data_models.input_models import Facility, Region, Instance, Resource, PatientsGroup, Activity, Pathway
 import geopandas as gpd
+
 
 specialities = ["CSC", "DERMA", "RHUMA", "URO", "GASTRO", "OPH", "ENDO", "ORL"]
 post_op_scenarios = {"DAY_HC":{"PTG_HC":28,"PTH_HC":21,"PTG_DOM":0,"PTH_DOM":0,"PTG_HC_HDJ":21,"PTH_HC_HDJ":14,"PTG_HDJ":0,"PTH_HDJ":0},
@@ -11,6 +11,8 @@ finance_PTG = {"CHIR/ORTHO":46, "ANES" : 46, "CSC": 34.75, "DERMA": 40,"ENDO": 4
                "OPH":40, "ORL":40, "RHUMA":40, "URO":40, "KINE_MCO":9.95, "DAY_HC":4365.61, "KINE_DOM":769.56, "KINE_SSR":126.80,"post_ORTH": 370.99}
 finance_PTH = {"CHIR/ORTHO": 46, "ANES": 46, "CSC":34.75, "DERMA":40, "ENDO":40, "GASTRO":40, "GYNECO":40, "OPH":40,"ORL": 40,
                "RHUMA":40, "URO":40, "KINE_MCO":9.95, "DAY_HC":3966.66, "KINE_DOM":803.30, "KINE_SSR":127.63, "post_ORTH":370.99}
+
+
 
 def load_types_parcours(path: str, min_patients: int, dep_code:str):
     """Read and filter TYPES_PARCOURS for Loire."""
@@ -59,44 +61,7 @@ def reduce_SSR_LOIRE(data: pd.DataFrame, dep_code) -> pd.DataFrame:
     return data[dept_code.isin(dep_code) & (data['GDE'] == "SSR_A")].reset_index(drop=True)
 
 
-import pandas as pd
-def get_Regions(df_instance: pd.DataFrame, dep_code: int) -> list[Region]:
-    """Creates Region instance (cantons) given a department code (cantons are grouped by bureau de voe)"""
-    gdf_cantons = get_geo_polygon()
-    gdf_geo = summarize_geo_data(gdf_cantons, get_pop65p(), dep_code)
-    list_regions = [Region(region_id="",
-                           coordinates="",
-                           facilities_affinity=_get_affinities()) for c in []]
-    return list_regions
 
-
-def _get_affinities():
-    return
-
-def get_Facilities(df_mco : pd.DataFrame, df_ssr : pd.DataFrame, dep_code: int, df_types_parcours_init: pd.DataFrame,
-                   df_types_parcours: pd.DataFrame, max_transferable_in: int = 10, max_transferable_out : int = 1) -> list[Facility]:
-    """Creates Facility objects corresponding to unique nofinesset ids """
-    list_facilities = []
-    gdf_cantons = get_geo_polygon()
-    gdf_geo = summarize_geo_data(gdf_cantons, get_pop65p(), dep_code)
-    df_finess = get_finness_info(df_mco, df_ssr, gdf_geo)
-    list_finess = list(df_finess["nofinesset"].unique())
-    list_finess.append("DOM")
-    m_hl = _get_resources_capacities(list_finess, df_types_parcours_init, df_ssr, df_mco, df_types_parcours)
-    for row in df_finess.itertuples():
-        
-        list_facilities.append(Facility(
-            facility_id = row.nofinesset,
-            facility_name = "" ,
-            region = row.can_code,
-            coordinates =[row.lat, row.lon] , 
-            resources_capacity = m_hl[row.nofinesset] ,
-            max_transferable_in = 0,
-            max_transferable_out = 0,
-            linked_facilities = [],
-            available_pathways= _get_available_pathways()))
-    #list_facilities = df_instance.apply(row_to_facility, axis=1).tolist()
-    return list_facilities
 
 def _get_available_pathways():
     """Returns available pathways for each facility ``type''"""
@@ -214,65 +179,6 @@ def _extend_nested_dict(init_dict: dict, ext_dict: dict):
     return res_dict
 
 
-def get_Instance(df_instance : pd.DataFrame) -> Instance:
-    """Returns object to store optimization instance parameters. Most variables are stores in a global config.yaml file """
-   
-    return Instance(
-            d_total = 0 ,
-            d_gr = get_demand_lower_bounds(),
-            under_q_g = {} ,
-            over_q_g = {},
-            under_q_gu = {},
-            over_q_gu = {},
-            p_transf = 0,
-            delta_l = 0,
-            alpha = 0
-        )
-
-
-def get_demand_lower_bounds() -> dict:
-    """ Returns ``d_gr'', the lower bound on patients asssigments per patient group, per canton"""
-    
-    return {}
-
-
-def get_Resources(df_instance : pd.DataFrame) -> list[Resource]:
-    """Creates Resource object with id for unique """
-    return [Resource(resource_id="")]
-
-
-def get_PatientGroups(df_instance: pd.DataFrame) -> list[PatientsGroup]:
-    """Creates PatientGroups """
-    list_patientsGroups = []
-    
-    for gid in group_ids:
-        possible_pathways = _get_possible_pathways(gid)
-        list_patientsGroups.append(PatientsGroup(group_id="", possible_pathways=possible_pathways))
-    return list_patientsGroups
-
-
-def _get_possible_pathways(gid):
-    """get list of possible passways for that group"""
-    return []
-
-    
-
-def get_Activities(df_instance : pd.DataFrame) -> list[Activity]:
-    """ get activities """
-    list_activities = [Activity(activity_id= "", associated_pathway="",
-                     associated_group="", transferable="",
-                     transfer_to="", required_resources={})]
-    return list_activities
-
-
-def get_PatientPathways(df_instance : pd.DataFrame) -> list[Pathway]:
-    """ get patients pathways"""                      
-    pathways = [Pathway(pathway_id= "p"+g, associated_group_id = g, quality_level = "0", list_activities= [],
-                         group_benefit = 1, list_next = []) for g in []]
-    return pathways
-
-
-
 
 def get_pop65p() -> pd.DataFrame:
     """ Returns a dataframe with the total population over 65 by Canton code and department code"""
@@ -339,3 +245,6 @@ def summarize_geo_data(gdf_cantons: gpd.GeoDataFrame, df_pop65p:pd.DataFrame, de
     gdf_geo["perc_65p"] = gdf_geo["pop65p"] / gdf_geo["population"] * 100
     gdf_geo["adjacent"] = gdf_geo.apply(lambda x: gdf_geo.loc[gdf_geo.geometry.touches(x.geometry),"can_code"].to_list(), axis=1)
     return gdf_geo
+
+def _get_region_affinities():
+    return
