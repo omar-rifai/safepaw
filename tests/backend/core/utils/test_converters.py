31945d9a2b107a3e11fba655b12490a89961d5fb
omar-rifai <omar.void@gmail.com>
2026-03-20 17:56:22 +0100

    Remove unused tests dir

--- tests/backend/core/utils/test_converters.py
+++
@@ -1,9 +0,0 @@
-from backend.core.mappers.input_mappers import convert_dm_to_json
-from tests.conftests import sample_params, sample_datamodel
-
-from pathlib import Path
-
-def test_convert_dm_to_json(sample_datamodel, sample_params):
-
-    system_params, _ = convert_dm_to_json(sample_datamodel)
-    assert system_params ==  sample_params
