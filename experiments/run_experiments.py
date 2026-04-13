e838b470485735001b038fd04d45ecbb39aed29b
omar-rifai <omar.void@gmail.com>
2026-04-13 13:19:42 +0200

    Update run_experiments

--- experiments/run_experiments.py
+++ experiments/run_experiments.py
@@ -6,23 +6,54 @@ from backend.core.main import run_driver
 from backend.core.utils.data_utils import get_results
 from pathlib import Path
 
-def experiment_exists(file_path, opt_params):
-    
+def experiment_exists(file_path: str, opt_params: dict)-> bool:
+    """Return True if the experiment already saved in file_path"""
     if not Path(file_path).is_file():
         return False
-
     df = pd.read_csv(file_path)
+    if not set(opt_params).issubset(df.columns):
+        return False
     mask = (df[list(opt_params)] == pd.Series(opt_params)).all(axis=1)
-    if mask.any():
-        return True 
-    return False
+    return mask.any()
+
 
 def write_entry(file_path, df):
-    if Path(file_path).isfile():
+    """Write or append entry to file"""
+    if Path(file_path).is_file():
         df.to_csv(file_path, mode="a", header = False, index=False)
     else:
         df.to_csv(file_path, mode="w", header = True, index=False)    
 
+
+def run_pthptg_instance(opt_params, files_paths):
+    """Run a single instance of the experiments with paramseters opt_params"""   
+    if experiment_exists(files_paths["main"], opt_params) and experiment_exists(files_paths["resources"], opt_params) and \
+        experiment_exists(files_paths["pathways"], opt_params):
+        return
+    
+    params_system = serialize_ptgpth_core(opt_params["dep_code"], opt_params["p_transf"], opt_params["p_orth"], opt_params["resources_mult"],save_params=True)
+
+    _, objective, dict_results = run_driver(params_system)
+    results = get_results(dict_results, params_system, objective)
+    results = results | opt_params
+    
+    base = {k: results[k] for k in ["dep_code", "p_transf", "p_orth", "resources_mult"]}
+
+    df_main = pd.DataFrame([{**base, "obj": results["obj"], "n_patients": results["n_patients"],
+                            "spi": results["spi"], "qci": results["qci"]}])
+    write_entry(files_paths["main"], df_main)
+    
+    df_res = pd.DataFrame([{**base, "resource": r, "usage": val}
+        for r, val in results["resources_usage"].items()
+    ])
+    write_entry(files_paths["resources"], df_res)
+
+    df_path = pd.DataFrame([{**base,"pathway": pth, "share": val}
+    for pth, val in results["pathway_distribution"].items()])
+    
+    write_entry(files_paths["pathways"], df_path)
+
+    return
 
 
 def run_pthptg_experiments(
@@ -32,7 +63,7 @@ def run_pthptg_experiments(
     mults= np.arange(1, 1.26, 0.05),
     clear_files = False
 ):
-    from pathlib import Path
+
     files_paths = { "main": "experiments/results_main_pthptg.csv", "resources": "experiments/results_resources_pthptg.csv",
     "pathways": "experiments/results_pathways_pthptg.csv"}
     if clear_files:
@@ -40,52 +71,17 @@ def run_pthptg_experiments(
             if os.path.exists(p):
                 os.remove(p)
 
-
     for d in dep_codes:
         for p_orth in ps_orths:
-            for p in ps_transfers:
-
-                opt_params = {"dep_code": d, "p_transf": 1, "p_orth": p_orth, "resources_mult": p}
-                
-                if experiment_exists(files_paths["main"], opt_params) and experiment_exists(files_paths["resources"], opt_params) and \
-                    experiment_exists(files_paths["pathways"], opt_params):
-                    continue
-                
-                params_system = serialize_ptgpth_core(opt_params["dep_code"], opt_params["p_transf"], opt_params["p_orth"], opt_params["resources_mult"],save_params=True)
-
-                _, objective, dict_results = run_driver(params_system)
-                results = get_results(dict_results, params_system, objective)
-                results = results | opt_params
-
-                df_main = pd.DataFrame([{"dep_code": results["dep_code"], "p_transf": results["p_transf"], "p_orth": results["p_orth"],
-                                         "resources_mult": results["resources_mult"], "obj": results["obj"], "n_patients": results["n_patients"],
-                                         "spi": results["spi"], "qci": results["qci"]}])
-                
-                df_res = pd.DataFrame([{"dep_code": results["dep_code"], "p_transf": results["p_transf"], "p_orth": results["p_orth"],
-                        "resources_mult": results["resources_mult"], "resource": r, "usage": val}
-                    for r, val in results["resources_usage"].items()
-                ])
-
-                df_path = pd.DataFrame([{"dep_code": results["dep_code"], "p_transf": results["p_transf"], "p_orth": results["p_orth"],
-                                     "resources_mult": results["resources_mult"],"pathway": pth, "share": val}
-                for pth, val in results["pathway_distribution"].items()])
-
-                write_entry(file_path, df)
-
-                if Path(files_paths["main"]).is_file():
-                    df_main.to_csv(main_path, mode="a", header=False, index=False)
-                else:
-                    df_main.to_csv(main_path, mode="w", header=True, index=False)
-
+            for p_transf in ps_transfers:
+                opt_params = {"dep_code": d, "p_transf": p_transf, "p_orth": p_orth, "resources_mult": 1}
+                run_pthptg_instance(opt_params, files_paths)
             
-
-            df_res.to_csv(res_path, mode="a", header=write_header, index=False)
-
+        for p_mult in mults:
+            opt_params = {"dep_code": d, "p_transf": 0, "p_orth": 0, "resources_mult": p_mult}
+            run_pthptg_instance(opt_params, files_paths)
             
-
-            df_path.to_csv(path_path, mode="a", header=write_header, index=False)
-
-            write_header = False  # only once
+    return 
 
 
 def run_experiements_burdett():
